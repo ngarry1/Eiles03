ee28b80a00232de153d02d1713fd19cdc036d463
ngarry1 <inilov@gmail.com>
2020-06-22 13:38:58 +0300

    [py007] Вывод на печать задачи 001 и рефакторинг проекта

--- runner.py
+++ runner.py
@@ -3,28 +3,9 @@
 
 # Created at 11.06.2020
 
-# from typing import List
-
-
-def print_task() -> None:
-    """Вывод описания задачи"""
-    QUESTION: str = 'Если выписать все натуральные числа меньше 10, кратные 3 или 5,\
-     то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.'
-    ANSWER: str = 'Найдите сумму всех чисел меньше 1000, кратных 3 или 5.'
-    TASK_CONST: list = [1000, 3, 5]
-#    print('Task const', TASK_CONST)
-    print(QUESTION)
-    print(ANSWER)
-
-
-def task01() -> int:
-    """Test"""
-    result: int = 0
-    print_task()
-    return result
+import func_task
 
 
 if __name__ == '__main__':
-    print('Результат: ', str(task01()))
-
+    print('Результат: ', str(func_task.task01()))
 
--- tasks/task01.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-d
-
-# Created at 11.06.2020
-
-
