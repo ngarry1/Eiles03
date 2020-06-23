40f01513c3977e22bd745d242f78b98888c3ed0e
ngarry1 <inilov@gmail.com>
2020-06-23 10:57:19 +0300

    [py012] Рефакторинг проекта

--- runner_task.py
+++ runner_task.py
@@ -3,11 +3,11 @@
 
 # Created at 11.06.2020
 
-import func_task
+import tasks
 from typing import Tuple
 
 task001_const: Tuple[int, int, int] = (10, 3, 5)
 
 if __name__ == '__main__':
-    print(func_task.task01.__doc__)
-    print('Результат: ', str(func_task.task01(task001_const)))
+    print(tasks.task01.__doc__)
+    print('Результат: ', str(tasks.task01(task001_const)))
