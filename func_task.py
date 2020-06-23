a81bea3f1e17cc68ab13e6818c1c541a8f69c877
ngarry1 <inilov@gmail.com>
2020-06-23 10:18:08 +0300

    [py010] Рефакторинг кода решения задачи 001

--- func_task.py
+++ func_task.py
@@ -5,27 +5,19 @@
 
 from typing import Tuple
 
-# Задача 001
-question: str = 'Задача номер 001: \n' + 'Если выписать все натуральные числа меньше 10,\
- кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.'
-answer: str = 'Найдите сумму всех чисел меньше 1000, кратных 3 или 5.\n'
-task001_const: Tuple[int, int, int] = (10, 3, 5)
 
+def task01(const_task: Tuple) -> int:
+    """Задача 001
 
-def task001(q: str, ans: str) -> None:
-    """Вывод на печать задачи 001"""
+    Если выписать все натуральные числа меньше 10,
+    кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+    Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+    """
 
-    print(q, ans)
-
-
-task001(question, answer)
-
-
-def task01() -> int:
-    """Решение задачи 001"""
+    # task001_const: Tuple[int, int, int] = (10, 3, 5)
 
     number_sum: int = 0
-    for num in range(3, task001_const[0]):
-        if (num % task001_const[1] == 0) or (num % task001_const[2] == 0):
+    for num in range(3, const_task[0]):
+        if (num % const_task[1] == 0) or (num % const_task[2] == 0):
             number_sum += num
     return number_sum
--- runner.py
+++ runner.py
@@ -4,8 +4,10 @@
 # Created at 11.06.2020
 
 import func_task
+from typing import Tuple
 
+task001_const: Tuple[int, int, int] = (10, 3, 5)
 
 if __name__ == '__main__':
-    print('Результат: ', str(func_task.task01()))
-
+    print(func_task.task01.__doc__)
+    print('Результат: ', str(func_task.task01(task001_const)))
