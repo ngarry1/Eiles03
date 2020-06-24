ef1b870263093b9d48b8d3b3a2b1e05ccc2cf024
ngarry1 <inilov@gmail.com>
2020-06-24 09:46:19 +0300

    [py016] Производительность задачи 002

--- func_measure.py
+++ func_measure.py
@@ -37,3 +37,35 @@ def task001(const_task: Tuple) -> int:
 
 
 task001((1000, 3, 5))  # Запуск
+
+
+@measure_time
+def task002(const_task: int) -> int:
+    """Задача 002
+
+    Каждый следующий элемент ряда Фибоначчи получается при сложении двух предыдущих.
+    Начиная с 1 и 2, первые 10 элементов будут:
+    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+    Найдите сумму всех четных элементов ряда Фибоначчи, которые не превышают четыре миллиона.
+    """
+
+    def fibonacci(n_f: int) -> int:
+        if n_f in (1, 2):
+            return 1
+        return fibonacci(n_f - 1) + fibonacci(n_f - 2)
+
+    def fib_sum() -> int:
+        fib_sum_even: int = 0
+        current_fib_number: int = 1
+
+        while fib_sum_even < const_task:
+            fib_number = fibonacci(current_fib_number)
+            if (fib_number % 2) == 0:
+                fib_sum_even += fib_number
+            current_fib_number += 1
+        return fib_sum_even
+
+    return fib_sum()
+
+task002(4000000)
